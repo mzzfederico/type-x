f5abfa70d8723f254e023f637f24f34f1049af5d
Federico Muzzo <federico.muzzo@bitrock.it>
2021-07-12 22:44:52 +0200

    Removed extra stuff that was not inteded to be ship with the engine

--- client/Tilesets/bricks.ts
+++
@@ -1,6 +0,0 @@
-import Tileset from "../@core/Assets/Tileset"
-import bricksSrc from "./bricks.png";
-
-const BricksTileset = new Tileset({ src: bricksSrc, width: 11, height: 3, tileSize: 16 });
-
-export default BricksTileset;
--- client/constants.ts
+++
@@ -1,4 +0,0 @@
-export const TILE_SIZE = 16;
-export const ROOM_WIDTH = 20;
-export const ROOM_HEIGHT = 12;
-export const ZOOM = 2;
